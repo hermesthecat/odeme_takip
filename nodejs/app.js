262200d0b781df31ed7727509442ffdfe5bf200d
hermesthecat <4947845+hermesthecat@users.noreply.github.com>
2025-02-27 21:06:17 +0300

    Add borsa routes to the application

--- nodejs/app.js
+++ nodejs/app.js
@@ -5,10 +5,12 @@ const port = 3000;
 const User = require('./models/User');
 const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin');
+const borsaRoutes = require('./routes/borsa');
 
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/admin', adminRoutes);
+app.use('/borsa', borsaRoutes);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
