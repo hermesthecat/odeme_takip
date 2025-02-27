3368025eaec5588c0d40f75f352102ab27547dde
hermesthecat <4947845+hermesthecat@users.noreply.github.com>
2025-02-27 21:33:35 +0300

    delete nodejs

--- nodejs/routes/currency.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/exchangeRate', async (req, res) => {
-  // Implement get exchange rate logic here
-  res.send('Get exchange rate endpoint - Not implemented');
-});
-
-module.exports = router;
