81490f66f7ffd5db1f1036a464b916e42dba1c1e
hermesthecat <4947845+hermesthecat@users.noreply.github.com>
2025-03-17 11:07:33 +0300

    Ödeme Yöntemi Güncelleme Modal'ında başlık ve etiketler dinamik hale getirildi. Ayrıca, güncelleme butonunun işlevi 'updateIncome()' yerine 'updateCard()' olarak değiştirildi.

--- modals/card_modal.php
+++ modals/card_modal.php
@@ -22,26 +22,26 @@
     </div>
 </div>
 
-<!-- Ödeme Yönetimi Güncelleme Modal -->
+<!-- Ödeme Yöntemi Güncelleme Modal -->
 <div class="modal fade" id="updateCardModal" tabindex="-1">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Ödeme Yöntemi Güncelle</h5>
+                <h5 class="modal-title"><?php echo t('card.modal.update_title'); ?></h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
             </div>
             <div class="modal-body">
                 <form id="updateCardForm">
                     <input type="hidden" id="update_card_id" name="id">
                     <div class="mb-3">
-                        <label class="form-label">Ödeme Yöntemi Adı</label>
+                        <label for="update_card_name" class="form-label"><?php echo t('card.name'); ?></label>
                         <input type="text" class="form-control" id="update_card_name" name="name" required>
                     </div>
                 </form>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"><?php echo t('cancel'); ?></button>
-                <button type="button" class="btn btn-primary" onclick="updateIncome()"><?php echo t('update'); ?></button>
+                <button type="button" class="btn btn-primary" onclick="updateCard()"><?php echo t('update'); ?></button>
             </div>
         </div>
     </div>
